78c108944ae33a2187ef67ad1da37cf490be6303
CHANDRESHEKAR-MG <Chandangowdachandu550@gmail.com>
2025-12-26 16:26:40 +0530

    add auth route

---
+++ routes/authRoute.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
