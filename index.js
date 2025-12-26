ec585ddf93ca29f85769c1a5f44db0cbca53416a
CHANDRESHEKAR-MG <Chandangowdachandu550@gmail.com>
2025-12-26 16:02:31 +0530

    add root route

--- index.js
+++ index.js
@@ -84,3 +84,8 @@ app.get("/dashboard", verifyToken, (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running successfully 🚀");
+});
